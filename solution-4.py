debf27d3e40b9c6e2f657871e4f8b82ba69d63e0
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2023-11-01 12:21:34 +0530

    Create solution-4.py
    solve : #4.Calculate and print the area of a rectangle when the user inputs its length and width.

---
+++ solution-4.py
@@ -0,0 +1,3 @@
+#4.Calculate and print the area of a rectangle when the user inputs its length and width.
+len, wid = float(input("Enter Lenght : ")), float(input("Enter Width : "))
+print("Area of rectangle : ",len*wid,"unit")

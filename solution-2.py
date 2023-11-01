c610ff2dd4fcf4495cd1377a792486fa3d6f0b0b
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2023-11-01 12:19:27 +0530

    Create solution-2.py
    solve : #2. Calculate the sum of two numbers entered by the user.

---
+++ solution-2.py
@@ -0,0 +1,3 @@
+#2.	Calculate the sum of two numbers entered by the user.
+a, b=int(input("Enter first Number : ")), int(input("Enter second Number : "))
+print("Sum of {} and {} is {}.".format(a, b, a+b))
